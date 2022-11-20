c5b3f7f5364298ee8a0b162877b74c1850e70b9d
Mirovesaram <88739188+Mirovesaram@users.noreply.github.com>
2022-11-19 22:23:12 -0300

    Add files via upload

---
+++ atividade11/src/main/java/Questao5/Carro.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Questao5;
+
+/**
+ *
+ * @author mimi
+ */
+public class Carro {
+    private String[] modelo={"Mazda","Aston Martin","Alfa Romeo","Porsche","Ferrari","Lamborghini","Audi"};
+    private String placa;
+    private String[] cor={"Vermelho","Cinza","Preto"};
+    public String[] getModelo()
+    {
+        return modelo;
+    }
+}
